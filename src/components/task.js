6c0e9438ee144d224526db8694320dc25fef80ca
Ian Middelkamp <ianmiddelkamp@gmail.com>
2020-03-10 07:37:17 -0400

    input change, ssave mock function and revert

--- src/components/task.js
+++ src/components/task.js
@@ -2,29 +2,62 @@ import React from 'react';
 import taskService from '../service/taskService'
 import { withRouter } from 'react-router'
 import DatePicker from "react-datepicker";
-
+import _ from 'lodash'
+import moment from 'moment'
 
 class Task extends React.Component {
 
      constructor(props) {
           super(props);
+       
           let match = props.match
           this.TaskId = match.params.TaskId;
-          this.task = taskService.getTask(this.TaskId);
+          let task = taskService.getTask(this.TaskId);
+          let taskBackup = _.clone(task)
+          this.state = {
+               task:task,
+               isSaved:true
+          }
           this.Statuses = taskService.getTaskStatuses();
           this.Priorities = taskService.getTaskPriorities();
-          this.handleSubmit = () => {
-
+          this.onChangeInput = (event) => {
+               let taskCopy = this.state.task
+               taskCopy[event.target.name] = event.target.value
+               this.setState({
+                    task:taskCopy,
+                    isSaved:false
+               })
           }
+        
           this.setDueDate = (date) => {
-
+               let mt = moment(date)
+               let taskCopy = this.state.task;
+               taskCopy.DueDate = mt.toDate();
+             
+               this.setState({      
+                    task:taskCopy,                  
+                    isSaved:false
+               })
           }
           this.getReadonly = () =>{
-               if(this.task.Status == "CLOSED" || this.task.Status == "COMPLETED"){
+               if(this.state.task.Status == "CLOSED" || this.state.task.Status == "COMPLETED"){
                     return true
                }else{ 
                     return false;
                }
+          }
+          this.revert = () => {  
+               this.setState({
+                    task:_.clone(taskBackup),
+                    isSaved:true
+               })
+          }
+          this.saveTask = () => {
+               task = this.state.task;
+               taskBackup = _.clone(task)
+               this.setState({
+                    isSaved:true
+               })
           }
           //  console.log(task)
      }
@@ -34,28 +67,29 @@ class Task extends React.Component {
      render() {
           let DueDateField = ""
           if(this.getReadonly()){
-               DueDateField =   <input className="form-control" name='DueDate' value={this.task.DueDate} readOnly/>
+               DueDateField =   <input className="form-control" name='DueDate' value={this.state.task.DueDate} readOnly/>
           }else{
-               DueDateField = <DatePicker
+               DueDateField = <div><DatePicker
                className="form-control"
-                    selected={this.task.DueDate}
+                    selected={this.state.task.DueDate}
                     onChange={date => this.setDueDate(date)}
                     showTimeSelect
                     timeFormat="HH:mm"
                     timeIntervals={15}
                     timeCaption="time"
-                    dateFormat="YYYY-MM-DD HH:mm:ss"
-               />
+                    dateFormat="yyyy-MM-dd HH:mm:ss"
+               /></div>
           }
 
           return (
-               <form onSubmit={this.handleSubmit}>
+               <form >
                     <div className="card">
                          <div className="card-header">
                               <input className="form-control" 
                               name='Title' 
                               type='text' 
-                              value={this.task.Title}
+                              value={this.state.task.Title}
+                              onChange={this.onChangeInput}
                               readOnly={this.getReadonly()}/>
                          </div>
                          <div className='card-body'>
@@ -65,7 +99,8 @@ class Task extends React.Component {
                                    <textarea 
                                    className="form-control"
                                    name='Notes' 
-                                   value={this.task.Notes} 
+                                   value={this.state.task.Notes} 
+                                   onChange={this.onChangeInput}
                                    readOnly={this.getReadonly()}>
 
                                    </textarea>
@@ -77,7 +112,9 @@ class Task extends React.Component {
                                                                       
                               </div>
                               <div className="form-group">
-                                   <select className="form-control" name='Status' value={this.task.Status} >
+                                   <label>Status</label>
+                                   <select className="form-control" name='Status' value={this.state.task.Status} 
+                                      onChange={this.onChangeInput}>
                                         {this.Statuses.map(X =>
                                              <option  key={X}  name={X} value={X}>{X}</option>
                                         )}
@@ -87,7 +124,7 @@ class Task extends React.Component {
                                    <label>CreationDate</label>
                                    <input className="form-control" 
                                    name='CreationDate'
-                                   value={this.task.CreationDate} 
+                                   value={this.state.task.CreationDate} 
                                    readOnly/>
 
                               </div>
@@ -95,13 +132,17 @@ class Task extends React.Component {
                                    <label>CompletionDate</label>
                                    <input className="form-control" 
                                    name='CompletionDate' 
-                                   value={this.task.CompletionDate}
+                                   value={this.state.task.CompletionDate}
                                    readOnly/>
 
                               </div>
                               <div className="form-group">
                                    <label>Priority</label>
-                                   <select className="form-control"  name='Priority' value={this.task.Priority} readOnly={this.getReadonly()}>
+                                   <select className="form-control"  
+                                        name='Priority' 
+                                        value={this.state.task.Priority} 
+                                        readOnly={this.getReadonly()}
+                                        onChange={this.onChangeInput}>
                                         {this.Priorities.map(X =>
                                              <option key={X} name={X} value={X}>{X}</option>
                                         )}
@@ -112,10 +153,15 @@ class Task extends React.Component {
                                    <label>Location</label>
                                    <input className="form-control" 
                                    name='Location' 
-                                   value={this.task.Location} 
-                                   readOnly={this.getReadonly()}/>
+                                   value={this.state.task.Location} 
+                                   readOnly={this.getReadonly()}
+                                   onChange={this.onChangeInput}/>
 
                               </div>
+                         </div>
+                         <div className='card-footer'>
+                              <button type='button' className='m-1 btn btn-success' disabled={this.state.isSaved} onClick={this.saveTask}>Save</button>
+                              <button type='button' className='m-1 btn btn-success' disabled={this.state.isSaved} onClick={this.revert}>Undo</button>
                          </div>
 
 
